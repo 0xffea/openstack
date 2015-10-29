ff1a124d4111618c59f0e865c9303a73196afb20
David Höppner <0xffea@gmail.com>
2015-10-29 15:47:52 +0100

    API

--- ubuntu-server-14.04/admin-openrc.sh
+++ ubuntu-server-14.04/admin-openrc.sh
@@ -5,4 +5,6 @@ export OS_TENANT_NAME=admin
 export OS_USERNAME=admin
 export OS_PASSWORD=openstack
 export OS_AUTH_URL=http://controller:35357/v3
+
 export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2
