78420493d1ecf34258bd78741ef9172482f73c2a
David Höppner <0xffea@gmail.com>
2015-10-29 15:33:45 +0100

    Glance

---
+++ ubuntu-server-14.04/install_glance.sh
@@ -0,0 +1,28 @@
+#
+# IMAGE SERVICE
+#
+mysql -uroot -popenstack <<EOL
+CREATE DATABASE glance;
+
+GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'localhost' \
+	  IDENTIFIED BY 'openstack';
+GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'%' \
+	  IDENTIFIED BY 'openstack';
+EOL
+
+source admin-openrc.sh
+
+openstack user create --domain default --password openstack glance
+openstack role add --project service --user glance admin
+
+openstack service create --name glance --description "OpenStack Image service" image
+openstack endpoint create --region RegionOne image public http://controller:9292
+openstack endpoint create --region RegionOne image internal http://controller:9292
+openstack endpoint create --region RegionOne image admin http://controller:9292
+
+su -s /bin/sh -c "glance-manage db_sync" glance
+
+service glance-registry restart
+service glance-api restart
+
+rm -f /var/lib/glance/glance.sqlite
