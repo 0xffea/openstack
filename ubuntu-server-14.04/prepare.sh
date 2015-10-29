e48e51f53b9a3130011c1a80173556bfccaa4cfd
David Höppner <0xffea@gmail.com>
2015-10-29 13:15:00 +0100

    ISO

--- ubuntu-server-14.04/prepare.sh
+++ ubuntu-server-14.04/prepare.sh
@@ -1,1 +1,6 @@
+wget http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-server-amd64.iso
+
+qemu-image create -f qcow2 openstack.img 10G
+
 kvm -m 1024 -smp 4 -cdrom ubuntu-14.04.3-server-amd64.iso -hda openstack.img -netdev user,id=user.0,hostfwd=tcp::2222-:22 -device e1000,netdev=user.0
+
--- ubuntu-server-14.04/start.sh
+++ ubuntu-server-14.04/start.sh
@@ -21,3 +21,19 @@ apt-get --yes install mariadb-server python-pymysql
 cp config/mysqld_openstack.cnf /etc/mysql/conf.d/
 service mysql restart
 
+#
+# KEYSTONE
+#
+mysql -uroot -popenstack <<EOL
+CREATE DATABASE keystone;
+
+GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' \
+	  IDENTIFIED BY 'openstack';
+GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' \
+	  IDENTIFIED BY 'openstack';
+EOL
+
+apt-get --yes install keystone apache2 libapache2-mod-wsgi  memcached python-memcache
+cp config/keystone.conf /etc/keystone/
+
+su -s /bin/sh -c "keystone-manage db_sync" keystone
