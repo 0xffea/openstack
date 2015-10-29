b5ebcc0042208ccf7ca30dd0473795cdf6ecaf85
David Höppner <0xffea@gmail.com>
2015-10-29 17:26:35 +0100

    Keystone users

--- ubuntu-server-14.04/install_keystone.sh
+++ ubuntu-server-14.04/install_keystone.sh
@@ -34,3 +34,16 @@ openstack service create --name keystone --description "OpenStack Identity" iden
 openstack endpoint create --region RegionOne identity public http://controller:5000/v2.0
 openstack endpoint create --region RegionOne identity internal http://controller:5000/v2.0
 openstack endpoint create --region RegionOne identity admin http://controller:35357/v2.0
+
+openstack project create --domain default --description "Admin Project" admin
+openstack user create --domain default  --password openstack admin
+openstack role create admin
+openstack role add --project admin --user admin admin
+
+openstack project create --domain default --description "Service Project" service
+
+openstack project create --domain default --description "Demo Project" demo
+openstack user create --domain default  --password openstack demo
+openstack role create user
+openstack role add --project demo --user demo user
+
