0640b0227de3091ea04af75af22dcade84a9e011
David Höppner <0xffea@gmail.com>
2015-10-30 11:10:53 +0100

    Start compute

---
+++ ubuntu-server-14.04/install_compute.sh
@@ -0,0 +1,25 @@
+#
+# COMPUTE
+#
+mysql -uroot -popenstack <<EOL
+CREATE DATABASE nove;
+
+GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'localhost' \
+	                  IDENTIFIED BY 'openstack';
+GRANT ALL PRIVILEGES ON nova* TO 'nova'@'%' \
+	                  IDENTIFIED BY 'openstack';
+EOL
+
+sh admin-openrc.sh
+
+openstack user create --domain default --password openstack nova
+openstack role add --project service --user nova admin
+
+openstack service create --name nova --description "OpenStack Compute" compute
+
+openstack endpoint create --region RegionOne compute public http://controller:8774/v2/%\(tenant_id\)s
+openstack endpoint create --region RegionOne compute internal http://controller:8774/v2/%\(tenant_id\)s
+openstack endpoint create --region RegionOne compute admin http://controller:8774/v2/%\(tenant_id\)s
+
+apt-get --yes install nova-api nova-cert nova-conductor nova-consoleauth nova-novncproxy nova-scheduler \
+	python-novaclient
