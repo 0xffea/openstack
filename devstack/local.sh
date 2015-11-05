9d41852041f112997656f0e83cade833ca837b02
David Höppner <0xffea@gmail.com>
2015-11-05 15:13:25 +0100

    Add nameserver 8.8.8.8 to private-subnet

--- devstack/local.sh
+++ devstack/local.sh
@@ -25,6 +25,8 @@ if is_service_enabled neutron; then
 
 	source $TOP_DIR/openrc demo demo
 
+	neutron subnet-update --dns-nameserver 8.8.8.8 private-subnet
+
 	neutron security-group-rule-create --protocol tcp      --port-range-min 22 --port-range-max 22 --direction ingress default
 	neutron security-group-rule-create --protocol icmp     --direction ingress --remote-ip-prefix 0.0.0.0/0 default
 
