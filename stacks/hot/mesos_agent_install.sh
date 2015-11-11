b6732af7dd77e9f926e21625ca37cf5bd5b38197
David Höppner <0xffea@gmail.com>
2015-11-11 16:55:36 +0000

    Move install agent script into separate file

--- stacks/hot/mesos_agent_install.sh
+++ stacks/hot/mesos_agent_install.sh
@@ -3,13 +3,10 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
 DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
 CODENAME=$(lsb_release -cs)
 echo "deb http://repos.mesosphere.com/${DISTRO} ${CODENAME} main" > /etc/apt/sources.list.d/mesosphere.list
-add-apt-repository -y ppa:webupd8team/java
-su -c 'echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections'
 apt-get --yes update
-apt-get --yes install oracle-java8-installer
-apt-get --yes install mesos marathon chronos
+apt-get --yes install mesos
+echo "$param_mesos_master_ip  mesosmaster" >> /etc/hosts
 mkdir -p /var/lib/mesos
 chown ubuntu:ubuntu /var/lib/mesos
 MY_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
-screen -dmS mesos-master bash -c  "/usr/sbin/mesos-master --ip=$MY_IP --work_dir=/var/lib/mesos"
-
+screen -dmS mesos-agent bash -c  "/usr/sbin/mesos-slave --master=mesosmaster:5050 --ip=$MY_IP --work_dir=/var/lib/mesos"
